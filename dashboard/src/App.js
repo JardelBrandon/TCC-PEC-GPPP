50306d9008646a0881c51bc2736b292e1356126e
JardelBrandon <jardelbrandon@gmail.com>
2023-11-13 19:47:53 -0300

    Cabeçalho e Rodapé
    Adicionando criação e estruturação do cabeçalho com a navegação e rodapé com informações sobre o projeto e contatos

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,27 +1,28 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
 function App() {
   return (
       <Router>
-          <ul>
-              <Link to ="/">Home</Link>
-              <Link to ="/contact">Contato</Link>
-              <Link to ="/company">Empresa</Link>
-              <Link to ="/newProject">Novo Projeto</Link>
-          </ul>
-              <Container customClass="min-height">
-                  <Routes>
-                      <Route exact path="/" element={<Home/>}/>
-                      <Route path="/company" element={<Company/>}/>
-                      <Route path="/contact" element={<Contact/>}/>
-                      <Route path="/newProject" element={<NewProject/>}/>
-                  </Routes>
-              </Container>
-          <p>Footer</p>
+          <Navbar />
+          <Container customClass="min-height">
+              <Routes>
+                  <Route exact path="/" element={<Home/>}/>
+                  <Route path="/company" element={<Company/>}/>
+                  <Route path="/projects" element={<Projects/>}/>
+                  <Route path="/contact" element={<Contact/>}/>
+                  <Route path="/newProject" element={<NewProject/>}/>
+              </Routes>
+          </Container>
+          <Footer />
       </Router>
   );
 }

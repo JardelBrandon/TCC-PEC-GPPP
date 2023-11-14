1d265d090c9eabd23248d7e8c1ae4e74ee47641f
JardelBrandon <jardelbrandon@gmail.com>
2023-11-14 12:03:58 -0300

    Mensagens do sistema
    Adicionando códigos responsáveis pela criação de mensagens com aprestação dos estados do projeto

--- dashboard/src/components/project/ProjectForm.js
+++ dashboard/src/components/project/ProjectForm.js
@@ -3,9 +3,10 @@ import styles from './ProjectForm.module.css'
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
-function ProjectForm({btnText}) {
+function ProjectForm({handleSubmit, btnText, projectData}) {
 
-    const[categories, setCategories] = useState([])
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -21,14 +22,34 @@ function ProjectForm({btnText}) {
             .catch((err) => console.log(err))
     }, [])
 
+    const submit = (e) => {
+        e.preventDefault()
+        // console.log(project)
+        handleSubmit(project)
+    }
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value})
+    }
+    function handleCategory(e) {
+        setProject({
+            ...project,
+            category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text,
+            }})
+    }
+
     return(
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <div>
                 <Input
                     type="text"
                     text="Nome do projeto"
                     name="name"
                     placeholder="Insira o nome do projeto"
+                    handleOnChange={handleChange}
+                    value={project.name ? project.name: ''}
                 />
             </div>
             <Input
@@ -36,11 +57,15 @@ function ProjectForm({btnText}) {
                 text="Orçamento do projeto"
                 name="budget"
                 placeholder="Insira o orçamento total"
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget: ''}
             />
             <Select
                 name="categoryId"
                 text="Selecione a categoria"
                 options={categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id: ''}
             />
             <SubmitButton text={btnText}/>
         </form>

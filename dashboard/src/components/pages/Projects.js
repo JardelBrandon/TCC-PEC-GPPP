f2020255d58754a2158a88d8996fd8d299e01549
JardelBrandon <jardelbrandon@gmail.com>
2023-11-14 12:17:22 -0300

    Container de Projetos
    Adicionando criação do container de projetos, possibilitando visualização dos projetos no dashboard

--- dashboard/src/components/pages/Projects.js
+++ dashboard/src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import {useLocation} from "react-router-dom";
 import Message from "../layout/Message";
+import styles from './Projects.module.css'
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 function Projects() {
 
@@ -10,9 +13,16 @@ function Projects() {
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.projectContainer}>
+            <div className={styles.titleContainer}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newProject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="sucess" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+
         </div>
     )
 }

1bea32965ca1c9bc361241bbf50efb540054cf06
JardelBrandon <jardelbrandon@gmail.com>
2023-12-09 19:26:31 -0300

    Dashboard - Gráficos
    Adicionando criação dos gráficos que serão utilizados para representação dos dados dos projetos e tarefas vindos do Notion API

--- dashboard/src/components/pages/Dashboard.tsx
+++ dashboard/src/components/pages/Dashboard.tsx
@@ -1,51 +1,177 @@
-import { useState } from "react";
+import { Chart } from "react-google-charts";
+import {useEffect, useState} from "react";
+import Loading from '../layout/Loading'
+import Container from "../layout/Container";
 
-// Copy the payload shape interface from our server
-// We want to copy (rather than import) since we we won't necessarily deploy our
-// front end and back end to the same place
-interface ThingToLearn {
-    label: string;
-    url: string;
-}
+// const projectsBudget = [
+//     ["Name", "Popularity"],
+//     ["Cesar", 250],
+//     ["Rachel", 0],
+//     ["Patrick", 2900],
+//     ["Eric", 0],
+// ];
+//
+// const projectsCosts = [
+//     ["Name", "Popularity"],
+//     ["Cesar", 370],
+//     ["Rachel", 600],
+//     ["Patrick", 0],
+//     ["Eric", 0],
+// ];
 
-function Dashboard() {
-    // A state value will store the current state of the array of data which can be updated
-    // by editing your database in Notion and then pressing the fetch button again
-    const [thingsToLearn, setThingsToLearn] = useState<ThingToLearn[]>([]);
+export const data = [
+    ["Element", "Density", { role: "style" }],
+    ["Copper", 8.94, "#b87333"], // RGB value
+    ["Silver", 10.49, "silver"], // English color name
+    ["Gold", 19.3, "gold"],
+    ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+];
 
+
+
+export const options = {
+    legend: { position: "top" },
+    title: "My Daily Activities",
+    is3D: true
+};
+
+export const optionsProjectsPie = {
+    legend: { position: "top" },
+    title: "Estados dos projetos",
+    subtitle: "Status",
+    colors: ["red", "gold", "green"],
+    is3D: true,
+};
+
+export const optionsProjectsBar = {
+    chart: {
+        legend: {position: "top"},
+        title: "Orçamento dos projetos",
+        subtitle: "Financeiro",
+    }
+};
+
+export const optionsTasksPie = {
+    legend: { position: "top" },
+    title: "Estados das tarefas",
+    subtitle: "Status",
+    colors: ["red", "green", "gold", "blue"],
+    is3D: true
+};
+
+export const optionsTasksBar = {
+    legend: { position: "top" },
+    title: "Orçamento das tarefas",
+    subtitle: "Financeiro",
+};
+
+export default function Dashboard() {
+
+    const [projects, setProjects] = useState([])
+    const [tasks, setTasks] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
+    useEffect( () => {
+            fetch('http://localhost:3333/projects/dashboard', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+    },[])
+
+    console.log(projects)
+
+    const projectsFinances = [["Projeto", "Capital", "Custos"]].concat(projects.map(project => ([
+            project.name, project.budget || 0, project.costs || 0
+        ]
+    )))
+
+    const projectsStatus = [["Status", "Projetos"]].concat(
+        [...projects.map(project => project.status).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())]
+    )
+
+    console.log(projectsFinances)
+    console.log(projectsStatus)
+
+    useEffect( () => {
+            fetch('http://localhost:3333/tasks/dashboard', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setTasks(data)
+                })
+                .catch((err) => console.log(err))
+    },[])
+    // setRemoveLoading(true)
+
+    const tasksFinances = [["Tarefa", "Custos"]].concat(tasks.map(task => ([
+            task.name, task.costs || 0
+        ]
+    )))
+
+    const tasksStatus = [["Status", "Projetos"]].concat(
+        [...tasks.map(task => task.status).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())]
+    )
+
+    console.log(tasksFinances)
+    console.log(tasksStatus)
+
+
+    if (!projects.length && !tasks.length) return <Container>{!removeLoading && <Loading />}</Container>;
     return (
-        <div>
-            <h1>Things to Learn</h1>
-            <button
-                type="button"
-                onClick={() => {
-                    fetch("http://localhost:5000")
-                        .then((response) => response.json())
-                        .then((payload) => {
-                            // Set the React state with the array response
-                            setThingsToLearn(payload);
-                            console.log(payload);
-                        });
-                }}
-            >
-                Fetch List
-            </button>
-
-            {/* Map the resulting object array into an ordered HTML list with anchor links */}
-            {/* Using index as key is harmless since we will only ever be replacing the full list */}
-            <ol>
-                {thingsToLearn.map((thing, idx) => {
-                    return (
-                        <li key={idx}>
-                            <a href={thing.url} target="_blank" rel="noopener noreferrer">
-                                {thing.label}
-                            </a>
-                        </li>
-                    );
-                })}
-            </ol>
-        </div>
-    );
+        <Container>
+            {removeLoading && projects.length === 0 && (
+                <p>Não há projetos cadastrados!</p>
+            )}
+            {projects.length && (
+                <Container>
+                    <Chart
+                        chartType="PieChart"
+                        data={projectsStatus}
+                        options={optionsProjectsPie}
+                        width={"100%"}
+                        height={"400px"}
+                    />
+                    <Chart
+                        chartType="Bar"
+                        width="100%"
+                        height="400px"
+                        data={projectsFinances}
+                        options={optionsProjectsBar}
+                    />
+                </Container>
+            )}
+            {tasks.length && (
+                <Container>
+                    <Chart
+                        chartType="PieChart"
+                        width={"100%"}
+                        height={"400px"}
+                        data={tasksStatus}
+                        options={optionsTasksPie}
+                    />
+                    <Chart
+                        chartType="ColumnChart"
+                        width="100%"
+                        height="400px"
+                        data={tasksFinances}
+                        options={optionsTasksBar}
+                    />
+                </Container>
+            )}
+        </Container>
+    )
 }
-
-export default Dashboard;
